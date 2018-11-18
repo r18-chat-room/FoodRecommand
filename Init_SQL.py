71c19d2ce9abee60769412b9f422cd802ed1588f
Mozart <31980988+LoveJTimberlake@users.noreply.github.com>
2018-11-18 10:41:40 +0800

    Update Init_SQL.py

--- Init_SQL.py
+++ Init_SQL.py
@@ -18,17 +18,24 @@ Tags_List = list()	#存储标签列表
 Food_List = list()
 #两者均要为string
 
-url = '/v1/internel/food/get-all-info'		#remains doubts
+url = '/v1/internel/food/get-all-info'		
 req = requests.get(url)
 all_info = req.json()
 all_food_info = all_info['foods']
 
 for each_info in all_food_info:
-	Food_List.append(each_info['id'])		#remained to change
+	Food_List.append(each_info['id'])		
+
+tag_url = '/v1/app/food/tag/random'
+d = {"count":10000}
+req = requests.post(tag_url,data = d)
+tag_list = req.json()['tags']
+for i in range(len(tag_list)):
+	Tag_List.append(tag_list['name'])
 
 ############################################ under is the code needed to be disabled after first usage #############################	
 	
-db = pymysql.connect(host = 'localhost',user = user_name,password = pw,db = 'db', charset = 'utf8mb4',cursorclass = pymysql.cursors.DictCursor)	#doubts			数据库权限初始化：grant all  on *.* to 'mozart'@'localhost' identified by 'mozewei19980206';
+db = pymysql.connect(host = 'localhost',user = user_name,password = pw,db = 'db', charset = 'utf8mb4',cursorclass = pymysql.cursors.DictCursor)	#doubts			数据库权限初始化：grant all  on *.* to 'user_name'@'localhost' identified by password;
 
 #1.用户标签表
 
@@ -133,11 +140,36 @@ cursor_9.execute("""Create Table Comment_Info(
 db.commit()
 cursor_9.close()
 
-
-#######################  above is the code needed to be disabled after first usage ###################################
-
 # put all initial food info in the table includng the comment 			 
+#分函数处理各个信息插入不同表中的行为
+def insert_into_FTs(info):	#info includes 	[{'food_id':..., 'tags':[{'name':...},...]},{...},]	 
 			 
+
+def insert_into_Comment_Info(info):    #info includes [{'food_id‘:, 'rate':, 'tags':[],'detail':[],'user_id':,}]
+		
+
+def insert_into_F_Aves(info):   #info is [{'food_id':,'rate':},...]
+	
+	
+def insert_into_User_CT(info):   #info is [{'user_id':,'comm_times':},...]
+	
+
+def insert_into_UFS_0(info):    #info is [{'user_id':,'food_id':,'rate':}]
+		
+		
+#roll over all_info dict and store data	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 			 
-			 
-			 
+#######################  above is the code needed to be disabled after first usage ###################################
